a6131f4041b236dafd1ac59628073a7fe936cf06
Rishabh <rishabhbhatt956@gmail.com>
2025-02-25 05:10:29 +0530

    Add JavaScript examples for basic operations and variable manipulation PENDING

---
+++ Master the Basics/Part1.js
@@ -0,0 +1,64 @@
+// Q. Sum of integer
+
+// let a = 10;
+// let b = 20;
+// console.log(a+b) // Addition
+
+// Q. Number and string;
+// let n = 12
+// let x = "14"
+// console.log(typeof(n+x)) // Concatination
+
+// Q. Sum and message;
+// let aa = 10;
+// let bb = 20;
+
+// console.log("Sum of 10 and 20 "+ aa+bb) 
+// console.log("Sum of 10 and 20 "+ (aa+bb)) // 30
+// 1 + 2+3 = 1020 (Concate)
+
+// console.log("1"-1) // TYPE COERCION.
+
+// Q. Accept and print the answer;
+
+// type casting: 
+// let age = Number(prompt("what is your age"));
+// console.log(typeof(age))
+// Prompt - To take value from user.
+
+// Q. Swap two variables via 3 methods;
+
+// 1st method:
+let a = 10;
+let b = 20;
+let c;
+
+// c = a; // c = 10, a = 10;
+// a = b; // a = 20, b = 20
+// b = c; // b =10
+// console.log(a);
+// console.log(b);
+
+// 2nd method:
+// a = a + b; // a = 30
+// b = a - b; // b = 10;
+// a = a - b
+
+// console.log(a,b)
+
+// 3rd method;
+// [a,b] = [b,a] // Destructuring
+// console.log(a,b)
+
+
+
+
+
+
+
+
+
+
+
+
+
