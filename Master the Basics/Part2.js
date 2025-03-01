ec7ff779ce8353c619e9c2ca0d4449002ac3ef25
Rishabh <rishabhbhatt956@gmail.com>
2025-03-01 12:42:04 +0530

    Add Part2.js with math functions and geometric calculations examples

---
+++ Master the Basics/Part2.js
@@ -0,0 +1,39 @@
+// Math functions
+
+// console.log(Math.round(10.6))
+// console.log(Math.ceil(10.1))
+// console.log(Math.floor(10.8))
+// console.log(Math.trunc(18.98))
+// console.log(Math.pow(2,5)) // 2^5
+// console.log(Math.sqrt(69))
+// console.log(Math.abs(-15)) // Negative to Positive
+// console.log(Math.max(23,53,25,6,2))
+// console.log(Math.trunc((Math.random()*9000)+1000)) // 0 to 999
+
+// let a = 34.45335; // how many digits you want after point.
+// console.log(typeof a.toFixed(2))
+
+
+// Q. Area of Rectrangle
+
+// let a = 5;
+// let b = 7;
+// console.log("Area of rectrangle: " + a*b)
+
+// Area of triangle by heron's formula
+// Square root of s * (s-a) * (s-b) * (s-c)
+
+// let a = 5;
+// let b = 4;
+// let c = 3
+
+// let s = (a+b+c)/2
+
+// console.log(Math.sqrt(s * (s-a) * (s-b) * (s-c)))
+
+// Q. Cicumferance of circle
+
+let r = 12;
+console.log(Number((2*Math.PI*r).toFixed(2)))
+
+
