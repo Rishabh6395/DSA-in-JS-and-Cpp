fbf1a091edb40b11cd521e6e2437430a57bfc765
Rishabh <rishabhbhatt956@gmail.com>
2025-03-04 06:48:10 +0530

    Add Loop.js with examples of for, while, and do-while loops, including a sum of n natural numbers

---
+++ Master the Basics/Video 3/Loop.js
@@ -0,0 +1,12 @@
+// loop
+// for while do-while loop
+
+// Q. Sum of n natural number;
+const n = 10; // user input
+let sum = 0;
+
+if(isNaN(n)) return
+for(let i = 1; i<=n; i++){
+    sum += i
+}
+console.log(sum)
