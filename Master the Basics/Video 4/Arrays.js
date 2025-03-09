9f8fa726b0326cb73ae3b50ad2631fe7830c905b
Rishabh <rishabhbhatt956@gmail.com>
2025-03-09 10:43:53 +0530

    Add Arrays.js with examples of array initialization and summation (PENDING)

---
+++ Master the Basics/Video 4/Arrays.js
@@ -0,0 +1,25 @@
+
+// let arr = new Array(3)
+// arr[0] = 12;
+// arr[1] = 13;
+// arr[2] = 14;
+// arr[6] = 20;
+
+// console.log(arr)
+
+
+
+// let arr = new Array(5)
+
+// for(let i = 0; i<arr.length; i++){
+//     arr[i] = Number(prompt("enter a value"))
+// }
+// console.log(arr)
+
+let arr = [10, 20, 30, 40, 50]
+let sum = 0;
+
+for(let i= 0; i<arr.length; i++){
+    sum = sum+arr[i]
+}
+console.log(sum)
