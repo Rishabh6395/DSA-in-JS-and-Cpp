4e140a7af192e1404ffef5478cd6f2d74a4814fa
Rishabh <rishabhbhatt956@gmail.com>
2025-03-11 05:08:11 +0530

    Update Arrays.js to include examples for finding the second maximum value in an array

--- Master the Basics/Video 4/Arrays.js
+++ Master the Basics/Video 4/Arrays.js
@@ -16,10 +16,38 @@
 // }
 // console.log(arr)
 
-let arr = [10, 20, 30, 40, 50]
-let sum = 0;
+// let arr = [10, 20, 30, 40, 50]
+// let sum = 0;
 
-for(let i= 0; i<arr.length; i++){
-    sum = sum+arr[i]
+// for(let i= 0; i<arr.length; i++){
+//     sum = sum+arr[i]
+// }
+// console.log(sum)
+
+
+// let arr = [11, 2, 3, 40, 34, 35, 3]
+// let min = arr[0]
+
+// for(let i =0; i<arr.length; i++){
+//     if(min>arr[i]){
+//         min = arr[i]
+//     }
+// }
+// console.log(min)
+
+
+let arr = [11, 2, 3, 40, 34, 35, 3]
+
+let max = Math.max(arr[0], arr[1]) // 11
+let sMax = Math.min(arr[0], arr[1]) // 2
+
+for(let i = 2; i<arr.length; i++){
+    if(arr[i] > max){
+        sMax = max;
+        max = arr[i]
+    }
+    else if(arr[i] > sMax && max !=arr[i]){
+        sMax = arr[i]
+    }
 }
-console.log(sum)
+console.log(sMax)
